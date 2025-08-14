3dca4e22a1b88dc52ffc8f9d65a9be5f0035aed5
betzrd <biaradadefreitas2001@gmail.com>
2025-08-14 19:55:58 -0300

    init coomit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+// Seção 06
+fun main() {
+
+    var teste: String = "Hello World"
+
+   println(teste)
+
+    // comentário em uma linha
+    /* comentário em várias linhas */
+    /**
+     * Comentário Documentação*/
+
+    /* Variável: funcionalidade que é capaz de armazenar um valor que pode ser usado durante a execução de um programa;
+    * Regras:
+    * 1) uma String é capaz de armazenar um texto;
+    * 2) var (variable);
+    * 3) uma variável tem 2 coisas obrigatórias: nome e tipo;
+    * 4) no exemplo em cima o nome da variável é "teste";
+    * 5) entre aspas duplas está sempre um texto;
+    * 6) não pode declarar a variável 2x;
+    * 7) as variáveis são únicas no programa;
+    * 8) nomeclatura das variáveis: não pode espaços em branco; caracteres especiais; não é recomendado acentos, palavras reservadas não podem definir uma variável {ex.: var; fun}
+    * 9) val: não altera - var: não altera;
+    * 10)*/
+}
